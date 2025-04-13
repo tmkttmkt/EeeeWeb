53b6b72a1eac1b2271ee98b9d7a651708a553e4f
TMKT <126658967+tmkttmkt@users.noreply.github.com>
2025-04-13 16:58:20 +0900

    kio

--- main/src/pages/login/Login.jsx
+++ main/src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { auth } from "../firebase";
+import { auth } from "../../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
